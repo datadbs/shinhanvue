178a574853371055390629089703cd32da812794
datadbs <dj9034kr@gmail.com>
2023-02-09 14:50:45 +0900

    4. 삭제 기능

--- vue/my-todo1/src/components/TodoMain.vue
+++ vue/my-todo1/src/components/TodoMain.vue
@@ -2,10 +2,14 @@
   <div class="page">
       <header><h1>Vue Fire todo1</h1></header>
       <main>
-        <div class="todos">
-          <div class="write">
+        <div class="todos"> 
+          <div class="write" v-if="writeState === 'add'">  <!-- 등록 -->
             <input ref="writeArea" type="text" v-model="addItemText"/>
             <button class="btn add" @click="addItem" @keyup.enter="addItem">Add</button>
+          </div>
+          <div class="write" v-else>  <!-- 수정 -->
+            <input ref="writeArea" type="text" v-model="editItemText"/>
+            <button class="btn add" @click="editSave" @keyup.enter="editSave">Save</button>
           </div>
           <ul class="list">
             <li v-for="(todo,i) in todos" :key="todo.text">
@@ -13,8 +17,8 @@
               <span>
                   {{todo.text}}
                 <b>
-                  <a href="">Edit</a>
-                  <a href="">Del</a>
+                  <a href="" @click.prevent="editShow(i)">Edit</a>
+                  <a href="" @click.prevent="del(i)">Del</a>
                 </b>
               </span>
             </li>
@@ -28,7 +32,10 @@
   export default {
       data() {
           return {
+              writeState: 'add',
               addItemText: '',
+              crrEditItem: '',
+              editItemText: '',
               todos:[
                   {text: `공부하기`, state: `yet`},
                   {text: '운동하기', state: 'done'},
@@ -49,6 +56,18 @@
           else {
             this.todos[i].state = 'yet'
           }
+        },
+        editSave() {
+          this.todos[this.crrEditItem].text = this.editItemText
+          this.writeState = 'add'
+        },
+        editShow(i) {
+          this.crrEditItem = i;
+          this.writeState = 'edit'
+          this.editItemText = this.todos[i].text
+        },
+        del(i) {
+          this.todos.splice(i,1)
         }
       },
       mounted() {
