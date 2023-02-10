0d85ec946edc30b2cea8cc0c16519196216f1967
datadbs <dj9034kr@gmail.com>
2023-02-10 16:17:29 +0900

    feat: add my-hacker

---
+++ vue/my-hacker/src/components/ListItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+    <ul class="list">
+      <li v-for="li in listData" :key="li.id">
+          <b>{{ li.time_ago }}</b>
+          <template v-if="$route.name === 'news'">
+            <span> by <router-link :to="`/user/${li.user}`">{{ li.user }}</router-link></span><br/>
+            <a :href="li.url">{{ li.title }}</a>
+            <router-link :to="`item?id=${li.id}`"><i>({{ li.comments_count }})</i></router-link>
+          </template>
+          <template v-else-if="$route.name === 'ask'">
+            <span> by <router-link :to="`/user/${li.user}`">{{ li.user }}</router-link></span><br/>
+            <router-link :to="li.url">{{ li.title }}<i>({{ li.comments_count }})</i></router-link>
+          </template>
+          <template v-else>
+            <span> by {{ li.user }}</span><br/>
+            <a :href="li.url">{{ li.title }}</a>
+          </template>
+      </li>
+    </ul>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['listData']
+  }
+  </script>
+  
+  <style>
+  
+  </style>
---
+++ vue/my-hacker/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    younjae
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  
+}
+</script>
